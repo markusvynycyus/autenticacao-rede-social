fa7d8fe8233ccd2cb1528b67c1958eb4352fe7f4
Marcos Venicios E. Chaves <markusvynycyus@hotmail.com>
2024-08-27 09:50:40 -0300

    Commit Inicial

---
+++ src/main/java/com/autenticacao_rede_social/AutenticacaoRedeSocialApplication.java
@@ -0,0 +1,13 @@
+package com.autenticacao_rede_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoRedeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoRedeSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autenticacao_rede_social/AutenticacaoRedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autenticacao_rede_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoRedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
